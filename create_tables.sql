7445ca0583a1277a06c727dc23124feb241a646c
Danilo-Sam <danilobrown17@gmail.com>
2024-09-03 14:22:34 -0300

    Correção no sql

--- create_tables.sql
+++ create_tables.sql
@@ -2,8 +2,8 @@
 CREATE TABLE tb_cliente (
     id INT PRIMARY KEY,
     nome VARCHAR(100),
-    cpf BIGINT,
-    tel BIGINT,
+    cpf VARCHAR(11),
+    tel VARCHAR(15),
     endereco VARCHAR(255),
     numero INT,
     cidade VARCHAR(100),
@@ -15,7 +15,7 @@ CREATE TABLE tb_cliente (
 -- Script SQL para criar a tabela tb_venda
 CREATE TABLE tb_venda (
     id INT PRIMARY KEY,
-    codigo VARCHAR(100),
+    codigo VARCHAR(100) UNIQUE,
     id_cliente_fk INT,
     valor_total DECIMAL(10, 2),
     data_venda DATE,
@@ -45,16 +45,6 @@ CREATE TABLE tb_produto_quantidade (
     FOREIGN KEY (id_venda_fk) REFERENCES tb_venda(id)
 );
 
-CREATE TABLE tb_venda (
-id INT PRIMARY KEY,
-codigo VARCHAR(100) UNIQUE,
-id_cliente_fk INT,
-valor_total DECIMAL(10, 2),
-data_venda DATE,
-status_venda VARCHAR(50),
-FOREIGN KEY (id_cliente_fk) REFERENCES tb_cliente(id)
-);
-
 create sequence seq_cliente
 start 1
 increment 1
@@ -75,46 +65,42 @@ start 1
 increment 1
 owned by tb_produto_quantidade.id;
 
-ALTER TABLE TB_CLIENTE
-ADD CONSTRAINT UK_CPF_CLIENTE UNIQUE (CPF);
+ALTER TABLE tb_cliente
+ADD CONSTRAINT UK_CPF_CLIENTE UNIQUE (cpf);
 
-ALTER TABLE TB_PRODUTO
-ADD CONSTRAINT UK_CODIGO_PRODUTO "CODIGO";
+ALTER TABLE tb_produto
+ADD CONSTRAINT UK_CODIGO_PRODUTO UNIQUE (codigo);
 
-ALTER TABLE TB_VENDA
-ADD CONSTRAINT UK_CODIGO_VENDA (CODIGO);
+ALTER TABLE tb_venda
+ADD CONSTRAINT UK_CODIGO_VENDA UNIQUE (codigo);
 
-SELECT V.ID AS ID.VENDA, V.CODIGO, V.ID_CLIENTE_FK, V.VALOR_TOTAL, V.DATA_VENDA, V.STATUS_VENDA,
-C.ID AS ID_CLIENTE, C.NOME, C.CPF, C.TELL, C.ENDERECO, C.NUMERO, C.CIDADE, C.ESTADO,
+SELECT V.ID AS ID_VENDA, V.CODIGO, V.ID_CLIENTE_FK, V.VALOR_TOTAL, V.DATA_VENDA, V.STATUS_VENDA,
+C.ID AS ID_CLIENTE, C.NOME, C.CPF, C.TEL, C.ENDERECO, C.NUMERO, C.CIDADE, C.ESTADO,
 P.ID AS ID_PROD_QTD, P.QUANTIDADE, P.VALOR_TOTAL AS PRODUTO_QTD_VALOR_TOTAL
-FROM TB_VENDA V
-INNER JOIN TB_CLIENTE C ON V.ID_CLIENTE_FK = C.ID
-INNER JOIN TB_PRODUTO_QUANTIDADE P ON P.ID_VENDA_FK = V.ID
+FROM tb_venda V
+INNER JOIN tb_cliente C ON V.ID_CLIENTE_FK = C.ID
+INNER JOIN tb_produto_quantidade P ON P.ID_VENDA_FK = V.ID
 WHERE V.CODIGO = 'A1';
 
 SELECT PQ.ID, PQ.QUANTIDADE, PQ.VALOR_TOTAL,
 P.ID AS ID_PRODUTO, P.CODIGO, P.NOME, P.DESCRICAO, P.VALOR
-FROM TB_PRODUTO_QUANTIDADE PQINNER JOIN TB_PRODUTO P ON P.ID = PQ.ID_PRODUTO_FK;
-
-CREATE SEQUENCE seq_cliente
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO CYCLE;
+FROM tb_produto_quantidade PQ
+INNER JOIN tb_produto P ON P.ID = PQ.ID_PRODUTO_FK;
 
 CREATE OR REPLACE PROCEDURE sp_cliente (
-    p_nome IN VARCHAR2,
-    p_cpf IN BIGINT,
-    p_tel IN BIGINT,
-    p_endereco IN VARCHAR2,
+    p_nome IN VARCHAR,
+    p_cpf IN VARCHAR,
+    p_tel IN VARCHAR,
+    p_endereco IN VARCHAR,
     p_numero IN INT,
-    p_cidade IN VARCHAR2,
-    p_estado IN VARCHAR2,
-    p_email IN VARCHAR2,
+    p_cidade IN VARCHAR,
+    p_estado IN VARCHAR,
+    p_email IN VARCHAR,
     p_idade IN INT
 )
-IS
+AS $$
 BEGIN
     INSERT INTO tb_cliente (id, nome, cpf, tel, endereco, numero, cidade, estado, email, idade)
-    VALUES (seq_cliente.NEXTVAL, p_nome, p_cpf, p_tel, p_endereco, p_numero, p_cidade, p_estado, p_email, p_idade);
+    VALUES (nextval('seq_cliente'), p_nome, p_cpf, p_tel, p_endereco, p_numero, p_cidade, p_estado, p_email, p_idade);
 END;
+$$ LANGUAGE plpgsql;
